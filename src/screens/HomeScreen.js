b7caad5174a8d37b5d6c67664084c2ff95de4b26
quynhdoan1912 <20520732@gm.uit.edu.vn>
2023-02-28 20:33:40 +0700

    Them chuc nang search nuoc

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,168 +1,261 @@
-import { StyleSheet, Text, View, StatusBar, TextInput, FlatList, Image, ScrollView, Dimensions, ImageBackground } from 'react-native'
-import React, { useEffect, useState, useRef } from 'react'
-import HomeHeadNav from '../components/HomeHeadNav'
-import Categories from '../components/Categories'
-import OfferSlider from '../components/OfferSlider'
-import { AntDesign } from '@expo/vector-icons';
-import { colors, veg, nonveg } from '../globals/style'
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+  Image,
+  ScrollView,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import HomeHeadNav from "../components/HomeHeadNav";
+import Categories from "../components/Categories";
+import OfferSlider from "../components/OfferSlider";
+import { AntDesign } from "@expo/vector-icons";
+import { colors, veg, nonveg } from "../globals/style";
 
-import { firebase } from '../../Firebase/firebaseConfig'
-import Cardslider from '../components/Cardslider'
-import BottomNav from '../components/BottomNav'
-import { windowHeight } from '../components/BottomNav'
-
+import { firebase } from "../../Firebase/firebaseConfig";
+import Cardslider from "../components/Cardslider";
+import BottomNav from "../components/BottomNav";
+import { windowHeight } from "../components/BottomNav";
 
 const HomeScreen = ({ navigation }) => {
-    const reff = useRef(null);
+  const [foodData, setFoodData] = useState([]);
+  const [teaData, setTeaData] = useState([]);
+  const [coffeeData, setCoffeeData] = useState([]);
+  const [toppingData, setToppingData] = useState([]);
 
-    const [foodData, setFoodData] = useState([]);
-    const [teaData, setTeaData] = useState([]);
-    const [coffeeData, setCoffeeData] = useState([]);
-    const [toppingData, setToppingData] = useState([]);
+  const foodRef = firebase.firestore().collection("FoodData");
 
+  useEffect(() => {
+    foodRef.onSnapshot((snapshot) => {
+      setFoodData(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
 
-    const foodRef = firebase.firestore().collection('FoodData');
+  useEffect(() => {
+    setTeaData(foodData.filter((item) => item.foodType == "Trà"));
+    setCoffeeData(foodData.filter((item) => item.foodType == "Coffee"));
+    setToppingData(foodData.filter((item) => item.foodType == "Topping"));
+  }, [foodData]);
+  // console.log(foodData)
+  // console.log(VegData)
 
-    useEffect(() => {
-        foodRef.onSnapshot(snapshot => {
-            setFoodData(snapshot.docs.map(doc => doc.data()))
-        }
-        )
-    }, [])
+  // search box
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        setTeaData(foodData.filter((item) => item.foodType == 'Trà'))
-        setCoffeeData(foodData.filter((item) => item.foodType == 'Coffee'))
-        setToppingData(foodData.filter((item) => item.foodType == 'Topping'))
-    }, [foodData])
-    // console.log(foodData)
-    // console.log(VegData)
+  // console.log(windowHeight)
+  const formatcurr = (n) => {
+    return String(
+      parseInt(n)
+        .toFixed(1)
+        .replace(/(\d)(?=(\d{3})+\.)/g, "$1,")
+    ).slice(0, -2);
+  };
 
+  return (
+    <View style={styles.container}>
+      <StatusBar />
 
+      <HomeHeadNav navigation={navigation} />
 
-    // search box
-    const [search, setSearch] = useState('')
+      <View style={styles.bottomnav}>
+        <BottomNav navigation={navigation} />
+      </View>
 
-    // console.log(search)
-    // console.log(windowHeight)
-    return (
-        <View style={styles.container}>
-            
-            <StatusBar />
-
-            <HomeHeadNav navigation={navigation} />
-
-            <View style={styles.bottomnav}>
-                <BottomNav navigation={navigation} />
-            </View>
-
-            
-            <ScrollView>
-                
-            <ImageBackground source={require('../../assets/950x350-meat-brown-solid-color-background.jpg')} resizeMode="repeat" style={styles.image}>
-                <View style={styles.searchbox}>
-                    <AntDesign name="search1" size={24} color="black" style={
-                        styles.searchicon
-                    } />
-                    <TextInput style={styles.input} placeholder="Tìm kiếm" onChangeText={(e) => {
-                        setSearch(e)
-                    }} />
-
-                </View>
-                {search != '' && //<View style={styles.seacrhresultsouter}>
-                    <ScrollView horizontal={true} style={{ width: "100%" }}>
-                    <FlatList style={styles.searchresultsinner} data={foodData} renderItem={
-                        ({ item }) => {
-                            if (item.foodName.toLowerCase().includes(search.toLowerCase())) {
-                                return (
-                                    <View style={styles.searchresult}>
-                                        <AntDesign name="arrowright" size={24} color="black" />
-                                        <Text style={styles.searchresulttext}>{item.foodName}</Text>
-                                    </View>
-                                )
-                            }
-                        }
-                    } />
-                {/* </View>} */}
-                </ScrollView>}
-                <Categories ref = {reff.current} />
-                <OfferSlider />
-                {/* <Text>HomeScreen</Text> */}
-
-                <Cardslider title={"Đặc biệt"} data={foodData} navigation={navigation} />
-                <Cardslider title={"Cà phê"} data={coffeeData} navigation={navigation} />
-                <Cardslider title={"Trà"} data={teaData} navigation={navigation} />
-                <Cardslider title={"Các thức uống khác"} data={toppingData} navigation={navigation} />
-                <Text> </Text>
-                <Text> </Text>
-                </ImageBackground>
+      <ScrollView>
+        <ImageBackground
+          source={require("../../assets/487498cf-8055-4faa-b835-710d05601f53.png")}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <View style={styles.searchbox}>
+            <AntDesign
+              name="search1"
+              size={24}
+              color="black"
+              style={styles.searchicon}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Search"
+              onChangeText={(e) => {
+                setSearch(e);
+              }}
+            />
+          </View>
+          {search != "" && ( //<View style={styles.seacrhresultsouter}>
+            <ScrollView horizontal={true} style={{ width: "100%" }}>
+              <FlatList
+                style={styles.searchresultsinner}
+                data={foodData}
+                renderItem={({ item }) => {
+                  if (
+                    item.foodName.toLowerCase().includes(search.toLowerCase())
+                  ) {
+                    const drink = () => {
+                      navigation.navigate("productpage", item);
+                    };
+                    return (
+                      <TouchableOpacity onPress={drink}>
+                        <View style={styles.searchresult}>
+                          <Image
+                            source={{ uri: item.foodImageUrl }}
+                            style={styles.cartimg}
+                          />
+                          <View style={{ flexDirection: "column" }}>
+                            <View style={styles.cartcardin}>
+                              <View style={styles.c1}>
+                                <Text style={styles.txt1}>{item.foodName}</Text>
+                              </View>
+                            </View>
+                            <View>
+                              <Text style={styles.txt2}>
+                                {formatcurr(item.foodPrice)} VNĐ/ly
+                              </Text>
+                            </View>
+                          </View>
+                        </View>
+                      </TouchableOpacity>
+                    );
+                  }
+                }}
+              />
+              {/* </View>} */}
             </ScrollView>
+          )}
+          <Categories />
+          <OfferSlider />
+          {/* <Text>HomeScreen</Text> */}
 
-            
-        </View>
-    )
-}
-
+          <Cardslider
+            title={"Special"}
+            data={foodData}
+            navigation={navigation}
+          />
+          <Cardslider
+            title={"Coffee"}
+            data={coffeeData}
+            navigation={navigation}
+          />
+          <Cardslider title={"Trà"} data={teaData} navigation={navigation} />
+          <Cardslider
+            title={"Topping"}
+            data={toppingData}
+            navigation={navigation}
+          />
+        </ImageBackground>
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        // marginTop: 50,
-        flex: 1,
-        backgroundColor: '#E5B73B',
-        
-        // alignItems: 'center',
-        width: '100%',
-        height: '100%',
-    },
-    image: {
-        flex: 1,
-        justifyContent: 'center',
-      },
-    searchbox: {
-        flexDirection: 'row',
-        width: '90%',
-        backgroundColor: colors.col1,
-        borderRadius: 30,
-        alignItems: 'center',
-        padding: 10,
-        margin: 20,
-        elevation: 10,
-    },
-    input: {
-        marginLeft: 10,
-        width: '90%',
-        fontSize: 18,
-        color: colors.text1,
-    },
-    searchicon: {
-        color: colors.cafe_title,
-    },
-    seacrhresultsouter: {
-        width: '100%',
-        marginHorizontal: 30,
-        height: '100%',
-        backgroundColor: colors.col1,
-    },
-    searchresultsinner: {
-        width: '100%',
-    },
-    searchresult: {
-        width: '100%',
-        flexDirection: 'row',
-        // alignItems: 'center',
-        padding: 5,
-    },
-    searchresulttext: {
-        marginLeft: 10,
-        fontSize: 18,
-        color: colors.text1,
-    },
-    bottomnav: {
-        position: 'absolute',
-        bottom: 0,
-        width: '100%',
-        backgroundColor: colors.col1,
-        zIndex: 20,
-    }
-})
-export default HomeScreen
+  container: {
+    // marginTop: 50,
+    flex: 1,
+    backgroundColor: colors.col1,
+
+    // alignItems: 'center',
+    width: "100%",
+    height: "100%",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  searchbox: {
+    flexDirection: "row",
+    width: "90%",
+    backgroundColor: colors.col1,
+    borderRadius: 30,
+    alignItems: "center",
+    padding: 10,
+    margin: 20,
+    elevation: 10,
+  },
+  txt2: {
+    marginTop: 5,
+    marginStart: 10,
+    fontSize: 16,
+    color: colors.text3,
+    fontWeight: "bold",
+  },
+  c1: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    borderRadius: 10,
+    padding: 5,
+  },
+  txt1: {
+    fontSize: 16,
+    color: colors.text1,
+    width: "200%",
+    fontWeight: "bold",
+  },
+  input: {
+    marginLeft: 10,
+    width: "90%",
+    fontSize: 18,
+    color: colors.text1,
+  },
+  cartcardin: {
+    flexDirection: "row",
+    margin: 5,
+    width: "58%",
+    alignItems: "center",
+    justifyContent: "center",
+    // backgroundColor: colors.text1,
+  },
+  searchicon: {
+    color: colors.cafe_title,
+  },
+  cartimg: {
+    marginLeft: 10,
+    marginRight: 10,
+    width: 140,
+    height: 140,
+    borderRadius: 10,
+    resizeMode: "center",
+  },
+  seacrhresultsouter: {
+    width: "100%",
+    marginHorizontal: 30,
+    height: "100%",
+    backgroundColor: colors.col1,
+  },
+  searchresulttextPrice: {
+    marginLeft: 10,
+    fontSize: 20,
+    color: "#CC0000",
+  },
+  searchresultsinner: {
+    width: 410,
+  },
+  searchresult: {
+    width: 410,
+    flexDirection: "row",
+    margin: 10,
+  },
+  searchresulttext: {
+    marginLeft: 10,
+    marginVertical: 10,
+    fontSize: 18,
+    color: "#873600",
+    flexDirection: "row",
+  },
+  bottomnav: {
+    marginTop: 20,
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: colors.col1,
+    zIndex: 20,
+  },
+});
+export default HomeScreen;
